9cb8ad6f3f5bea46ba5497fdc68cfa11f5430297
Melcn <melaniearinosy@outlook.com>
2023-04-27 11:59:06 +0200

    var type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,47 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit{
 
-  public isActive : boolean = true;
+  // public isActive : boolean = true;
 
   ngOnInit(): void {
     
   }
   
-  Click(){
+  // Click(){
     
-    this.isActive = !this.isActive;
-  }
+  //   this.isActive = !this.isActive;
+  // }
+
+  public fruits: string[] = [
+    "Pommes",
+    "Poires",
+    "Bananes",
+    "Kiwi",
+  ];
+
+
+  public myFirstStr: string ="This is a string";
+  public mySecondStr: string ="This is a string";
+
+  public myNum : number = 42;
+  public myNegativeNum: number = -42;
+  public myDecimal: number = 5.4;
+
+  public myFirstBool : boolean = true;
+  public mySecondBool : boolean = false;
+
+  public myNull : null = null;
+
+  public myArray : [] = [];
+
+  public myStrArray: String[] = ["str1", 'str2', `str3`];
+
+  public myObject: {} = {};
+
+  public myJoker: any;
+
+  public myFirstJokerArray: any[] = ["str1", 'str2', `str3`, 42, true, [], {}];
+  public mySecondJokerArray: Array<any> = ["str1", 'str2', `str3`, 42, true, [], {}];
+
+
 }
